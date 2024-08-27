f29f43056a15ad86baa3241f21239d26069b426a
vrk2230 <ram.koushik17@gmail.com>
2024-08-27 22:36:54 +0100

    new

--- day-8-dependent-block/main.tf
+++ day-8-dependent-block/main.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "name" {
   ami           = "ami-0a2202cf4c36161a1"
   instance_type = "t2.micro"
   key_name      = "Newkey"
+  depends_on = [ aws_s3_bucket.name ]
 }
 
 resource "aws_s3_bucket" "name" {
   bucket     = "vrknewbucketawstraining123"
-  depends_on = [aws_instance.name]
 }
