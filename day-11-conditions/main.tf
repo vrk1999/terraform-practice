3922e9affca4bf24f7384e76a8aa52317a84605f
vrk2230 <ram.koushik17@gmail.com>
2024-09-02 19:55:21 +0100

    latest

---
+++ day-11-conditions/main.tf
@@ -0,0 +1,68 @@
+ resource "aws_instance" "myec2" {
+     ami = "ami-0230bd60aa48260c6"
+     instance_type = "t2.micro"
+     count = 2
+     tags = {
+     #   Name = "webec2"
+       Name = "webec2-${count.index}"
+     }
+ }
+
+# example-2 with variables list of string 
+
+variable "ami" {
+  type    = string
+  default = "ami-0440d3b780d96b29d"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "sandboxes" {
+  type    = list(string)
+  default = [ "sandbox_server_two", "sandbox_server_three"]
+}
+
+# main.tf
+resource "aws_instance" "sandbox" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  count         = length(var.sandboxes)
+
+  tags = {
+    Name = var.sandboxes[count.index]
+  }
+}
+
+
+ variable "user_names" {
+   description = "IAM usernames"
+   type        = list(string)
+   default     = ["user1", "user2", "user3"]
+ }
+ resource "aws_iam_user" "example" {
+   count = length(var.user_names)
+   name  = var.user_names[count.index]
+ }
+ 
+
+ variable "ami" {
+   type    = string
+   default = "ami-0230bd60aa48260c6"
+ }
+ variable "instance_type" {
+   type = string
+   default = "t2.nano"
+ }
+
+ # main.tf
+ resource "aws_instance" "dev" {
+   ami           = var.ami
+   instance_type = var.instance_type
+   count         = var.instance_type == "t2.micro" ? 1 : 0
+   tags = {
+     Name = "dev_server"
+   }
+ }
